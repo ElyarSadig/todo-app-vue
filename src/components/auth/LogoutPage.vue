58ae206b443e81eeb5c2d55aaed41cf823afecdf
elyar <elyarnejati@gmail.com>
2024-08-22 19:16:42 +0330

    feat: add LogoutPage

---
+++ src/components/auth/LogoutPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { onMounted } from "vue";
+import { useTodoStore } from "@/stores/todo";
+import { useAuthStore } from "@/stores/auth";
+import { useRouter } from "vue-router";
+
+const todoStore = useTodoStore();
+const authStore = useAuthStore();
+const router = useRouter();
+
+onMounted(async () => {
+  await todoStore.clearTodos();
+  await authStore.destroyToken();
+  router.push({ name: "home" });
+});
+</script>
