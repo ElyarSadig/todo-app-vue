806fcdd4a2d76b3e4b05df82b94155334acf3236
elyar <elyarnejati@gmail.com>
2024-08-25 20:04:09 +0330

    feat: add todo item remaining component

---
+++ src/components/TodoItemsRemaining.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useTodoStore } from "@/stores/todo";
+import { computed } from "vue";
+
+const store = useTodoStore();
+
+const remaining = computed(() => {
+  return store.remaining;
+});
+</script>
+
+<template>
+  <div>{{ remaining }} items left</div>
+</template>
