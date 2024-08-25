090b0f5e6777c21b5fda9ac6f272055f27587a3b
elyar <elyarnejati@gmail.com>
2024-08-25 20:12:47 +0330

    feat: add todo filtered

---
+++ src/components/TodoFiltered.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useTodoStore } from "@/stores/todo";
+import { computed } from "vue";
+
+const store = useTodoStore();
+const filter = computed(() => {
+  return store.filter;
+});
+const changeFilter = () => {
+  store.updateFilter(filter);
+};
+</script>
+
+<template>
+  <div>
+    <button :class="{ active: filter == 'all' }" @click="changeFilter('all')">
+      All
+    </button>
+    <button
+      :class="{ active: filter == 'active' }"
+      @click="changeFilter('active')"
+    >
+      Active
+    </button>
+    <button
+      :class="{ active: filter == 'completed' }"
+      @click="changeFilter('completed')"
+    >
+      Completed
+    </button>
+  </div>
+</template>
