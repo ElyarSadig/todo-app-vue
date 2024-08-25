be395b90c37e62bbbf2f49fe49f578eea1a33585
elyar <elyarnejati@gmail.com>
2024-08-25 20:22:18 +0330

    feat: add todo clear completed

---
+++ src/components/TodoClearCompleted.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useTodoStore } from "@/stores/todo";
+import { computed } from "vue";
+
+const store = useTodoStore();
+const showClearCompletedButton = computed(() => {
+  return store.showClearCompletedButton;
+});
+const clearCompleted = () => {
+  store.clearCompleted();
+};
+</script>
+
+<template>
+  <button v-if="showClearCompletedButton" @click="clearCompleted">
+    Clear Completed
+  </button>
+</template>
